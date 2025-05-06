d3e3d4f22520de6bc1c9b94abcb5982717b10b6d
Phlask <killercursed@gmail.com>
2025-05-05 23:34:43 -0400

    added court_words function

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    path = "books/frankenstein.txt"
+    text = get_book_text(path)
+    num_words = count_words(text)
+    print(f"{num_words} words found in the document")
 
 def get_book_text(path):
     with open(path) as f:
         contents = f.read()
     return contents
+
+def count_words(text):
+    word_list = text.split()
+    return len(word_list)
 
 main()
